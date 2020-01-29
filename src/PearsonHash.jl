f1284d247536eff34d15324de07185e10571fea5
Kyle Daruwalla <daruwalla@wisc.edu>
2020-01-29 10:04:41 -0600

    Initial package complete.

--- src/PearsonHash.jl
+++ src/PearsonHash.jl
@@ -1,5 +1,36 @@
 module PearsonHash
 
-greet() = print("Hello World!")
+using Random: shuffle!
+export hash8, hashn
+
+_table = shuffle!(collect(0:255))
+
+function hash8(data::Array{UInt8}, seed::UInt8 = zero(UInt8))
+    h = seed
+    for byte in data
+        h = _table[xor(h, byte) + 1]
+    end
+
+    return h
+end
+hash8(data::AbstractString, seed::UInt8 = zero(UInt8)) = hash8(Vector{UInt8}(data), seed)
+
+"""
+    hashn(data, n, seed::UInt8 = 0)::UInt128
+
+Compute a (`8*n`)-bit hash for data (optionally specify `seed`).
+"""
+function hashn(data::Array{UInt8}, n::Integer, seed::UInt8 = zero(UInt8))
+    (n > 16) && @warn "hashn does not support n > 16 (results may be wrong)"
+
+    h = UInt128(0)
+    for i in 0:(n - 1)
+        firstbyte = UInt8((data[1] + i) % 256)
+        h |= hash8([firstbyte; data[2:end]], seed) << (i * 8)
+    end
+
+    return h
+end
+hashn(data::AbstractString, n::Integer, seed::UInt8 = zero(UInt8)) = hashn(Vector{UInt8}(data), n, seed)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
+using Random
+Random.seed!(1234)
+
 using PearsonHash
 using Test
 
 @testset "PearsonHash.jl" begin
-    # Write your own tests here.
+    @test hash8("Test") == 77
+    @test hashn("Test", 2) == 0x0000000000000000000000000000424d
+    @test hashn("Test", 4) == 0x0000000000000000000000001f6a424d
+    @test_logs (:warn, "hashn does not support n > 16 (results may be wrong)") (hashn("Test", 17); true)
 end
